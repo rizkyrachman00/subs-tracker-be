886be65e25fd3f5b5765479f05751199497fea15
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-02-12 16:47:58 +0700

    feat(db): add database Mongodb connection file using Mongoose

--- app.js
+++ app.js
@@ -3,12 +3,23 @@
 import express from "express";
 import { PORT } from "./config/env.js";
 
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+
 const app = express();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 // const port = 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Subscription running on port http://localhost:${PORT}`);
+
+  await connectToDatabase();
 });
 
 app.get("/", (req, res) => {
