e821848c05b4cd38b6d899d40e28af8d369d1b7b
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-01 00:37:36 +0700

    feat(auth): add package jsonwebtoken & bcrypt, add API routes function auth (sign-up & sign-in), function store data to DB

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => res.send({ title: "Sign up" }));
-authRouter.post("/sign-in", (req, res) => res.send({ title: "Sign in" }));
-authRouter.post("/sign-out", (req, res) => res.send({ title: "Sign out" }));
+// authRouter.post("/sign-up", (req, res) => {
+//   //req.body contains the parsed JSON payload sent by the client
+//   // for example... {email: "..." , password}
+// });
+
+// -> /api/v1/auth/sign-up -> POST BODY -> {name, email, password} -> Create a new user
+
+// Path: /api/v1/auth/sign-up (POST)
+authRouter.post("/sign-up", signUp);
+
+// Path: /api/v1/auth/sign-in (POST)
+authRouter.post("/sign-in", signIn);
+
+// Path: /api/v1/auth/sign-out (POST)
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
