8e6359c145fdd0faaadd628af91e68bb48330181
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-02 01:36:44 +0700

    feat: add middleware auth for user permission to get user by id

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
 
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => {
   res.send({ title: "CREATE new user" });
